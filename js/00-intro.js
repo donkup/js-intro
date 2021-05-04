f74d2ec8b631202aa58ee0de3aadc21abfb87fd2
Donatas <donkuprys@gmail.com>
2021-05-04 19:01:22 +0300

    Initial commit

---
+++ js/00-intro.js
@@ -0,0 +1,19 @@
+// single-line coment
+
+/*
+multi
+line 
+coment
+*/
+
+/*
+KONCEPTAI
+
+1) Kintamieji - isimena informacija
+2) Salygos sakiniai - tapusavio palyginimui
+3) Ciklai - darbas su pasikartojancio formato informacija, pasikartojanciu procesu valdymas
+4) Funkcijos - uzdara logika kuria galima perpanaudoti pagal poreiki
+5) Ivykiai - pvz( uzsikrove puslapis; paspaude peles mygtuka; pradejo siusti faila; issiunte uzklausa ir t.t.t.)
+6) oop - objektinis programavimas
+
+*/
