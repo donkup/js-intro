a20218eff4f4918b1d707c0acb99c26794c36261
Donatas <donkuprys@gmail.com>
2021-05-07 20:10:28 +0300

    Auto, multi, day, even

---
+++ js/18-even.js
@@ -0,0 +1,13 @@
+function even(sk) {
+    if (sk % 2 === 0) {
+        return true        
+    }
+    return false
+    //return true jeigu lyginis
+    //return false  jeigu nelyginis
+}
+
+console.log(even(5));
+console.log(even(2));
+console.log(even(8));
+console.log(even(3));
