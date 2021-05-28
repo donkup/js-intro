a1bb94caa6bcb29c8bde2eefa8ffeb15fe5faee9
Donatas <donkuprys@gmail.com>
2021-05-28 20:32:32 +0300

    map filter reduce sort

--- js/23-map.js
+++ js/23-map.js
@@ -10,3 +10,17 @@ function dvigubinu(list) {
 }
 module.exports = dvigubinu;
 
+
+//idex.js
+// const dvigubinu = require('./js/23-map');
+
+// const ilgiai = [1, 2, 3, 4, 5];
+
+
+// const duIlgiai = dvigubinu(ilgiai);
+// console.log(ilgiai);
+// console.log(duIlgiai);
+
+// const trysIlgiai = ilgiai.map(item => item * 3);
+// console.log(trysIlgiai);
+
